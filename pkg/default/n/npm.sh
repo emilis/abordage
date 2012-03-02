4cbddb50f9706bf70b7c5008d2765a70db396fc8
Emilis Dambauskas <emilis.d@gmail.com>
2012-03-02 22:31:32 +0200

    Added local install command for npm.

--- pkg/default/n/npm.sh
+++ pkg/default/n/npm.sh
@@ -4,13 +4,26 @@
 
 install_global() {
 
-    pkg/default/n/nodejs.sh install_global
-    sudo apt-get install npm;
+    if hash npm 2>&-;
+    then echo "NPM is already installed."
+    else
+        pkg/default/n/nodejs.sh install_global
+        sudo apt-get install npm;
+    fi
+}
+
+install() {
+
+    if hash npm 2>&-;
+    then echo "NPM is already installed."
+    else
+        curl http://npmjs.org/install.sh | sh
+    fi
 }
 
 ### Main: ---------------------------------------------------------------------
 
 case "$1" in
-    install_global) "$1" ;;
+    install|install_global) "$1" ;;
     *)  echo "Command $0 $1 is not supported." ;;
 esac
