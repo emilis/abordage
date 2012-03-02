f3e5e82b716294316ffa040efea4bc2627225c2d
Emilis Dambauskas <emilis.d@gmail.com>
2012-03-02 18:42:20 +0200

    Rewrote bootstrap.sh to use git if possible.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,13 +1,36 @@
 #!/bin/sh
 
-TARBALL="$HOME/abordage-latest.tar.gz";
+### Variables: ----------------------------------------------------------------
+
 DIR="$HOME/abordage";
+
+### Functions: ----------------------------------------------------------------
+
+command_exists() {
+    
+    hash $1 2>&-;
+}
+
+git_extract() {
+    
+    git clone "https://emilis@github.com/emilis/abordage.git" "$DIR"
+}
+
+tar_extract() {
+
+    tarball="$HOME/abordage-latest.tar.gz";
+    wget -O "$tarball" "https://github.com/emilis/abordage/tarball/master";
+    tar xzvf "$tarball";
+}
+
+### Main: ---------------------------------------------------------------------
 
 cd "$HOME";
 
-wget -O "$TARBALL" https://github.com/emilis/abordage/tarball/master;
-
-tar xzvf "$TARBALL";
+if command_exists git;
+    then git_extract
+    else tar_extract
+fi
 
 cd "$DIR";
 
