a8e263c801146ae40cc2094ad7a27c07b2eaeb62
Emilis Dambauskas <emilis.d@gmail.com>
2012-03-05 11:31:06 +0200

    Added tasks to update abordage and set-up private repository.

--- bin/abordage.sh
+++ bin/abordage.sh
@@ -1,11 +1,45 @@
 #!/bin/sh
 
+### Functions: ----------------------------------------------------------------
+
+setup_private() {
+
+    if [ -d "pkg/private" ];
+    then echo "Private repo already set-up";
+    else
+        curdir=`pwd -P`;
+        cd pkg
+        git clone "$1" private
+        cd "$curdir";
+    fi
+}
+
+
+update() {
+
+    git pull
+    
+    if [ -d "pkg/private" ];
+    then
+        curdir=`pwd -P`;
+        cd pkg/private;
+        git pull
+        cd "$curdir";
+    fi
+}
+
 ### Main: ---------------------------------------------------------------------
 
 case "$1" in
     setup)
         echo "Congratulations on installing Abordage!";
         echo "Usage: bin/arr {install|install-global} PACKAGE";
+        ;;
+    setup-private)
+        setup_private "$2"
+        ;;
+    update)
+        update
         ;;
     *)
         echo "Abordage repository management is not yet working."
