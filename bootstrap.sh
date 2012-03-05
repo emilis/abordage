72a55fc70a3a791c2c4cee1aa55bfbfded718416
Emilis Dambauskas <emilis.d@gmail.com>
2012-03-05 11:41:23 +0200

    Rewrote scripts to reflect new bin/ structure.

--- bootstrap.sh
+++ bootstrap.sh
@@ -34,5 +34,5 @@ fi
 
 cd "$DIR";
 
-. bin/abordage setup;
+. bin/abordage.sh setup;
 
--- pkg/default/a/abordage.sh
+++ pkg/default/a/abordage.sh
@@ -1,35 +1,49 @@
 #!/bin/sh
 
-### Variables: ----------------------------------------------------------------
-
-PREFIX="/usr";
-LIB="$PREFIX/lib";
-DIR="$LIB/abordage";
-BIN="$PREFIX/bin";
-
 ### Functions: ----------------------------------------------------------------
 
-install_global() {
+_install_tasks() {
 
     if [ -d "$DIR" ];
     then echo "$DIR already exists.";
-    else sudo git clone "https://github.com/emilis/abordage.git" "$DIR";
+    else git clone "https://github.com/emilis/abordage.git" "$DIR";
     fi
 
     if [ -f "$BIN/arr" ];
     then echo "$BIN/arr already exists.";
-    else sudo ln -s "$LIB/abordage/bin/global/arr" "$BIN/arr";
+    else ln -s "$DIR/bin/arr" "$BIN/arr";
     fi
 
     if [ -f "$BIN/abordage" ];
     then echo "$BIN/abordage already exists.";
-    else sudo ln -s "$LIB/abordage/bin/global/abordage" "$BIN/abordage";
+    else ln -s "$DIR/bin/abordage" "$BIN/abordage";
     fi
+}
+
+install_global() {
+
+    PREFIX="/usr";
+    LIB="$PREFIX/lib";
+    DIR="$LIB/abordage";
+    BIN="$PREFIX/bin";
+
+    sudo _install_tasks
+}
+
+install() {
+
+    . pkg/default/h/home-bin.sh install
+
+    PREFIX="$HOME/local";
+    DIR="$PREFIX/abordage";
+    BIN="$HOME/bin";
+
+    _install_tasks
 }
 
 ### Main: ---------------------------------------------------------------------
 
 case "$1" in
-    install_global) "$1" ;;
+    install|install_global) "$1" ;;
     *) exit 127 ;;
 esac
