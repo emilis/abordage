eecd1331452e950436a003fce1fc43202fdf10bd
Emilis Dambauskas <emilis.d@gmail.com>
2012-03-07 18:03:14 +0200

    Added exit status checking and proper error reporting to arr.sh.

--- bin/arr.sh
+++ bin/arr.sh
@@ -17,13 +17,27 @@ get_pkg_script() {
     fi
 }
 
+check_task_status() {
+
+    case $1 in
+        127)
+            echo "The package does not support task $2." >&2;
+            ;;
+        0)
+            ;;
+        *)
+            echo "An error occured while installing your package." >&2;
+            ;;
+    esac
+}
+
 ### Main: ---------------------------------------------------------------------
 
 get_pkg_script "$2"
 
 if [ -z "$pkg_script" -o ! -f "$pkg_script" ];
 then
-    echo "The script $pkg_script for the package $2 does not exist."
+    echo "The script $pkg_script for the package $2 does not exist." >&2;
     exit 127;
 fi
 
@@ -31,16 +45,18 @@ case $1 in
 
     install|install-local)
         "$pkg_script" install;
+        check_task_status $? install
     ;;
 
     install-global)
         if [ $(id -u) -ne 0 ];
         then 
-            echo "Root privileges required for \"install-global\" task."
-            echo "Please try \"sudo $0 $@\"";
+            echo "Root privileges required for \"install-global\" task." >&2;
+            echo "Please try \"sudo $0 $@\"" >&2;
             exit 126;
         else
             "$pkg_script" install_global;
+            check_task_status $? install_global
         fi
     ;;
 
