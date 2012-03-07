c610d6d61b573f3fbc67efe8f26590c0e2f542ef
Emilis Dambauskas <emilis.d@gmail.com>
2012-03-07 15:57:09 +0200

    Added a lib for platform-independent package manager usage.

---
+++ include/pkg_manager.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+. include/platform.sh
+
+### Functions: ----------------------------------------------------------------
+
+install_package() {
+
+    if [ -z "$1" ];
+    then
+        echo "Usage: install_package PKG_NAME";
+        return 1;
+    elif [ -z "$platform_pkg_manager" ];
+    then
+        echo "Package manager not detected."
+        return 1;
+    else
+        "$platform_pkg_manager" install "$1"
+    fi
+}
+
+
+### Main: ---------------------------------------------------------------------
+
+case "$1" in
+    install|install-package)
+        install_package "$2"
+        ;;
+esac
