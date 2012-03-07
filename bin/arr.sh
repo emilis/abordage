a2e6f5f3f35e5c0a5beafd3f09db57cf698557e6
Emilis Dambauskas <emilis.d@gmail.com>
2012-03-07 17:00:14 +0200

    Changed install script checking order: first private repo, then default.

--- bin/arr.sh
+++ bin/arr.sh
@@ -10,10 +10,10 @@ get_pkg_script() {
     default_pkg="pkg/default/$first_char/$name.sh"
     private_pkg="pkg/private/$first_char/$name.sh"
 
-    if [ -f "$default_pkg" ]
-        then pkg_script="$default_pkg"
-    elif [ -f "$private_pkg" ]
+    if [ -f "$private_pkg" ]
         then pkg_script="$private_pkg"
+    elif [ -f "$default_pkg" ]
+        then pkg_script="$default_pkg"
     fi
 }
 
